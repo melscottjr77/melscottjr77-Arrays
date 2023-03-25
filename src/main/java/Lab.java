d8cbe23c72810591fd502ed2b25bdb93dcbaf091
melscottjr77 <melscottjr77@gmail.com>
2023-03-25 20:06:24 +0000

    Completed Lab Arrays

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 /**
  * An array is a group of some predefined number of elements.
@@ -15,21 +18,19 @@ public class Lab {
      * @return the length of arr.
      */
     public int getLengthOfArray(int[] arr){
-        return 0;
+        return arr.length;
     }
-
-
-
     /**
      * @param arr the array we're getting data from.
+     * 
      * @param n the index (number) of the element we're looking to return.
      *          Indexes in arrays start counting from zero.
      * @return the element at index n of arr.
      */
-    public int getNthElementOfArray(int[] arr, int n){
-        return 0;
+    public int getNthElementOfArray(int[] arr, int n){  
+        return arr[n];
     }
-
+    
     /**
      *
      * @param arr the array we're getting data from.
@@ -38,15 +39,15 @@ public class Lab {
      * @param val the value that we will assign to a spot in the array.
      * @return nothing. Because of pass-by-reference, any change to the array will be reflected across our java program.
      */
+
     public void setNthElementOfArray(int[] arr, int n, int val){
-
+        arr[n] = val;
     }
-
     /**
      * @param n the size of the array we wish to create
      * @return a new int array that can contain n elements.
      */
     public int[] returnNewArraySizeN(int n){
-        return null;
+        return IntStream.range(0, n).toArray();    
     }
 }
